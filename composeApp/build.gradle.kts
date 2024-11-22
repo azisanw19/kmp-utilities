a888c5b1514363d199ef7c34fc9c015e6aab3524
Aziz Anwar <azisanw19@gmail.com>
2024-11-22 23:12:09 +0700

    1.0.0-alpha01

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,21 +1,11 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import java.util.Properties
-import java.io.FileInputStream
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
-}
-
-val applicationProperties = Properties()
-val applicationPropertiesFile = rootProject.file("maven.properties")
-
-if (applicationPropertiesFile.exists()) {
-    applicationProperties.load(FileInputStream(applicationPropertiesFile))
 }
 
 kotlin {
--- core/utilities/time/build.gradle.kts
+++ core/utilities/time/build.gradle.kts
@@ -1,6 +1,4 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import com.vanniktech.maven.publish.SonatypeHost
 import java.util.Properties
 import java.io.FileInputStream
 
@@ -9,6 +7,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.vanniktechMavenPublish)
 }
 
 val applicationProperties = Properties()
@@ -55,5 +54,56 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
     defaultConfig.minSdk = libs.versions.android.minSdk.get().toInt()
     buildFeatures.compose = true
-
 }
+
+val mavenProperties = Properties()
+val mavenPropertiesFile = rootProject.file("maven.properties")
+
+if (mavenPropertiesFile.exists()) {
+    mavenProperties.load(FileInputStream(mavenPropertiesFile))
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "id.feinn.azisanw19",
+        artifactId = "feinn-date-time",
+        version = "1.0.0-alpha01"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("KMP Utilities Date Time")
+        description.set("FeinnDateTime is a custom library that enables date and time management in Android and iOS applications")
+        inceptionYear.set("2024")
+        url.set("https://github.com/azisanw19/kmp-utilities")
+
+        licenses {
+            license {
+                name.set("Apache License 2.0")
+                url.set("https://github.com/azisanw19/kmp-utilities/blob/main/LICENSE")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("azisanw19")
+                name.set("Aziz Anwar")
+                email.set("azisanw19@gmail.com")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/azisanw19/kmp-utilities")
+        }
+    }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
+}
+
+task("testClasses") {}
