44cffc6d8cf47998b3a05d00903480afcc8495af
Aziz Anwar <azisanw19@gmail.com>
2025-06-27 09:13:00 +0700

    fix: renamed unit test byte buffer

--- core/utilities/byteBuffer/src/commonTest/kotlin/id/feinn/utility/bytebuffer/FeinnByteBufferTest.kt
+++ core/utilities/byteBuffer/src/commonTest/kotlin/id/feinn/utility/bytebuffer/FeinnByteBufferTest.kt
@@ -2,12 +2,12 @@ package id.feinn.utility.bytebuffer
 
 import kotlin.test.Test
 
-class AccessibilityTest {
+class FeinnByteBufferTest {
 
     private val byteBuffer = FeinnByteBuffer.wrap(byteArrayOf(72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100))
 
     @Test
-    fun `test get byteBuffer`() {
+    fun `get byteBuffer`() {
         byteBuffer.get()
     }
 
