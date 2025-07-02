012fb1dc4bca480a46c36516cb5cfed24ef1560f
Aziz Anwar <azisanw19@gmail.com>
2025-07-02 16:12:02 +0700

    feat: screenshot but not perfect in ios

--- core/components/screenshot/src/androidMain/kotlin/id/feinn/components/screenshot/FeinnScreenshot.android.kt
+++ core/components/screenshot/src/androidMain/kotlin/id/feinn/components/screenshot/FeinnScreenshot.android.kt
@@ -1,25 +1,58 @@
 package id.feinn.components.screenshot
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.view.View
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.geometry.Rect
-import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.drawToBitmap
+
 
 internal actual class FeinnTakeScreenshot(
-    private val view: View
+    private val context: Context
 ) {
     actual fun takeScreenshot(
-        bounds: Rect,
-        bitmapCallback: (FeinnScreenshotResult) -> Unit
+        modifier: Modifier,
+        bitmapCallback: (FeinnScreenshotResult) -> Unit,
+        size: CoordinateSize,
+        content: @Composable () -> Unit,
     ) {
-        view.screenshot(bounds, bitmapCallback)
+        val composeView = ComposeView(context).apply {
+            setContent {
+                Box(
+                    modifier = modifier
+                ) {
+                    content()
+                }
+            }
+        }
+
+        val widthPx = size.width.toInt()
+        val heightPx = size.height.toInt()
+        val widthSpec = View.MeasureSpec.makeMeasureSpec(widthPx, View.MeasureSpec.EXACTLY)
+        val heightSpec = View.MeasureSpec.makeMeasureSpec(heightPx, View.MeasureSpec.EXACTLY)
+
+        composeView.measure(widthSpec, heightSpec)
+        composeView.layout(0, 0, widthPx, heightPx)
+
+        val bitmap: Bitmap = composeView.drawToBitmap()
+        val imageBitmap: ImageBitmap = bitmap.asImageBitmap()
+
+        bitmapCallback(
+            FeinnScreenshotResult.Success(imageBitmap)
+        )
     }
 }
 
 @Composable
 internal actual fun rememberFeinnTakeScreenshot(): FeinnTakeScreenshot {
-    val view = LocalView.current
+    val context = LocalContext.current
 
-    return remember { FeinnTakeScreenshot(view) }
+    return remember { FeinnTakeScreenshot(context) }
 }
