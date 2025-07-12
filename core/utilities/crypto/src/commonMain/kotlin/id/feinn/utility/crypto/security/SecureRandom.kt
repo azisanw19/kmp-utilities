dd3e67a5c9e1e212542a3de3bbbe6d678ad35ea7
Aziz Anwar <azisanw19@gmail.com>
2025-07-12 11:29:37 +0700

    feat: SecureRandom nextBytes random not secure

--- core/utilities/crypto/src/commonMain/kotlin/id/feinn/utility/crypto/security/SecureRandom.kt
+++ core/utilities/crypto/src/commonMain/kotlin/id/feinn/utility/crypto/security/SecureRandom.kt
@@ -1,4 +1,21 @@
 package id.feinn.utility.crypto.security
 
+import kotlin.random.Random
+
+/**
+ * A mock implementation of SecureRandom for Kotlin Multiplatform.
+ *
+ * This class provides a method to fill a given [ByteArray] with pseudo-random signed bytes.
+ *
+ * ⚠️ WARNING: This is **not cryptographically secure**.
+ * It uses [kotlin.random.Random], which is suitable only for non-security purposes such as simulations,
+ * testing, or placeholder randomness.
+ *
+ */
 public class SecureRandom {
+
+    public fun nextBytes(bytes: ByteArray) {
+        for (i in bytes.indices) bytes[i] = Random.nextInt(Byte.MIN_VALUE.toInt(), Byte.MAX_VALUE.toInt() + 1).toByte()
+    }
+
 }
